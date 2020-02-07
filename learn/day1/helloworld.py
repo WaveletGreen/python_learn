b50c861ce8d697c5b32d07bda690df6911f97fbe
WaveletGreen <1243093769@qq.com>
2020-02-07 11:27:46 +0800

    Initial commit

---
+++ learn/day1/helloworld.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'fancyears'
+__mtime__ = '2020/2/7'
+# code is far away from bugs with the god animal protecting
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃      ☃      ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛
+"""
+
+def helloworld():
+    print ("你好，世界")
